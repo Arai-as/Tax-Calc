4c0945b280462a81b24d35e0b1ebc8f1cc4abe8a
Yuki Arai <61082585+Arai-as@users.noreply.github.com>
2020-02-16 15:54:31 +0900

    Add files via upload

---
+++ tax-func.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    
+$('.btn').on('click',function(){
+    var sum=0;
+    var salaly;
+    var TaxBorder=1030000;
+    var minasBorder;
+    var count=0;
+    var wage=parseInt($('#wage').val());
+    var moneySpace;
+    var timeH;
+    if(!wage){
+        alert("時給を入力してください");
+        exit;
+    }
+    $('.base-money').each(function(){
+        salaly=$(this).val();
+        if(!salaly){
+            salaly=0;
+            count=count+1;
+        }
+        sum=sum+parseInt(salaly);
+    });
+    minasBorder=TaxBorder-sum;
+    moneySpace=minasBorder/count;
+    timeH=moneySpace/wage;
+    $('.result-sum').text(sum);
+    $('.Bminas').text(minasBorder);
+    $('.time-m').text(timeH);
+});
+});
